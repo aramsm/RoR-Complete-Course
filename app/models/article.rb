b088d429d74d454f4fa82ca847e4726f6390039e
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-03-21 14:16:52 -0300

    Created new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, length: { minimum: 3, maximum: 50 }
-  validadtes :description, length: { minimum: 10 }
+  validates :description, length: { minimum: 10 }
 end
