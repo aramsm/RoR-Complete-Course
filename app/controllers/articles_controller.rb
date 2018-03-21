5317b910581952baca5b5208b6f10a0219228762
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-03-21 15:46:01 -0300

    Added flash messages, show template and fixed create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,17 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
+    if @article.save
+    	flash[:success] = 'Article was successfully created'
+	    redirect_to article_path(@article)
+	  else
+	  	flash.now[:danger] = @article.errors.full_messages.join("<li>").html_safe
+	  	render :new
+	  end
+	end
 
-	  redirect_to articles_show_path(@article) if @article.save
+	def show
+		@article = Article.find(params[:id])
 	end
 
 	private
