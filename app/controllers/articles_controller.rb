a3a0f245063b856039e8523ab7b3abe4c851d974
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-04-02 12:36:08 -0300

    Fixed identation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,57 +1,57 @@
 class ArticlesController < ApplicationController
-	before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-  	@articles = Article.all
+    @articles = Article.all
   end
 
-	def new
-		@article = Article.new
-	end
+  def new
+    @article = Article.new
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def create
-		@article = Article.new(article_params)
+  def create
+    @article = Article.new(article_params)
     if @article.save
-    	flash[:success] = 'Article was successfully created'
-	    redirect_to article_path(@article)
-	  else
-	  	flash.now[:danger] = @article.errors.full_messages.join("<li>").html_safe
-	  	render :new
-	  end
-	end
+      flash[:success] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      flash.now[:danger] = @article.errors.full_messages.join("<li>").html_safe
+      render :new
+    end
+  end
 
-	def update
-		if @article.update(article_params)
-    	flash[:success] = 'Article was successfully updated'
-	    redirect_to article_path(@article)
-	  else
-	  	flash.now[:danger] = @article.errors.full_messages.join("<li>").html_safe
-	  	render :edit
-	  end
-	end
+  def update
+    if @article.update(article_params)
+      flash[:success] = 'Article was successfully updated'
+      redirect_to article_path(@article)
+    else
+      flash.now[:danger] = @article.errors.full_messages.join("<li>").html_safe
+      render :edit
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def destroy
-		if @article.destroy
-			flash[:success] = 'Article successfully deleted'
-		else
-			flash[:danger] = 'Article could not be deleted'
-		end
+  def destroy
+    if @article.destroy
+      flash[:success] = 'Article successfully deleted'
+    else
+      flash[:danger] = 'Article could not be deleted'
+    end
 
-		redirect_to articles_path
-	end
+    redirect_to articles_path
+  end
 
-	private
-	  def article_params
-	  	params.require(:article).permit(:title, :description)
-	  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
-	  def set_article
-	  	@article = Article.find(params[:id])
-	  end
+    def set_article
+      @article = Article.find(params[:id])
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-	def index
-	end
+  def index
+  end
 
-	def about
-	end
+  def about
+  end
 end
