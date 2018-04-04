88437b206e2aec3a0eac6078f7fa3c2cb36bae8c
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-04-04 15:30:32 -0300

    Added association and debuger on layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first #placeholder
+
     if @article.save
       flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
