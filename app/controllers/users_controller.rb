e8607ea79e662b37e73c4ee2e5b85572ddff9011
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-04-04 23:20:47 -0300

    Added edit feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -15,7 +16,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Your account was successfully updated'
+      redirect_to articles_path
+    else
+      flash[:danger] = @user.errors.full_messages.join("<li>").html_safe
+      render :edit
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
